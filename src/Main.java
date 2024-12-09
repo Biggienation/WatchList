e5b5848d7f96fd4aa02c37a0066461dd521812cb
Biggienation <alexpet5888@gmail.com>
2024-12-09 15:39:07 +0100

    CRUD almost done

--- src/Main.java
+++ src/Main.java
@@ -21,10 +21,76 @@ public class Main {
             switch (input) {
                 case 1 -> showAllMovies();
                 case 2 -> showAllGenres();
+                case 3 -> showFavorites();
                 case 4 -> inputAddMovie();
-                case 5 -> inputDeleteMovie();
+                case 5 -> inputAddGenre();
+                case 6 -> inputDeleteMovie();
+                case 7 -> inputDeleteGenre();
                 case 0 -> on  = false;
             }
+        }
+    }
+
+    private static void printConsole() {
+        System.out.println("\n Choose: \n");
+        System.out.println(
+                        "1  - Show all watched movies \n" +
+                        "2  - Show genres watched\n" +
+                        "3  - Show favorites\n" +
+                        "4  - Add a movie\n" +
+                        "5  - Add genre\n" +
+                        "6  - Delete a movie\n" +
+                        "7  - Delete genre\n" +
+                        "8  - Search\n" +
+                        "0  - Exit"
+        );
+    }
+
+    private static int consoleInput() {
+        int input = 0;
+        while (true) {
+            try {
+                input = Integer.valueOf(scanner.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Please choose one of the following options:");
+                printConsole();
+            }
+        }
+        return input;
+    }
+
+    //Show methods
+private static void showAllMovies() {
+    String sql = "SELECT movies.movieId, movies.movieTitle," +
+                "movies.movieDirector, movies.movieReleaseYear," +
+                "movies.movieFavorite, genres.genreName\n" +
+                "FROM movies\n" +
+                "LEFT JOIN genres\n" +
+                "ON genreID = movieGenreId";
+
+    try{
+        Connection connection = connect();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        printAllMovies(resultSet);
+
+    } catch (SQLException e){
+        System.out.println(e.getMessage());
+    }
+
+}
+
+    private static void printAllMovies(ResultSet resultSet) throws SQLException {
+        System.out.println();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("movieId") + "\t"
+                    + resultSet.getString("movieTitle") + "\t"
+                    + resultSet.getString("movieDirector") + "\t"
+                    + resultSet.getString("movieReleaseYear") + "\t"
+                    + resultSet.getString("movieFavorite") + "\t"
+                    + resultSet.getString("genreName"));
         }
     }
 
@@ -51,9 +117,72 @@ public class Main {
         }
     }
 
+    private static void showFavorites() {
+
+    }
+
+    //Add methods
+    private static void inputAddMovie() {
+        // Add genre option
+        try {
+            System.out.println("Enter movie name: ");
+            String title = scanner.nextLine();
+            System.out.println("Enter movie director: ");
+            String director = scanner.nextLine();
+            System.out.println("Enter movie year: ");
+            int year = Integer.parseInt(scanner.nextLine());
+            System.out.println("enter movie genre: ");
+            String genre = scanner.nextLine();
+            addMovie(title, director, year, genre);
+        } catch (Exception e) {
+            System.out.println("Woops, something went wrong.");
+        }
+    }
+
+    private static void addMovie(String title, String director, int year, String genre) {
+
+        String sql = "INSERT INTO movies (movieTitle, movieDirector, movieReleaseYear, movieFavorite, movieGenreId)" +
+                " VALUES (?,?,?,'NO',(SELECT genreId FROM genres WHERE genreName = ?))";
+
+        try{
+            Connection connection = connect();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, title);
+            preparedStatement.setString(2, director);
+            preparedStatement.setInt(3, year);
+            preparedStatement.setString(4, genre);
+            preparedStatement.executeUpdate();
+            System.out.println("The movie was successfully added to the database.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private static void inputAddGenre() {
+        System.out.println("Enter genre name: ");
+        String genreName = scanner.nextLine();
+        addGenre(genreName);
+    }
+
+    private static void addGenre(String genreName) {
+        String sql = "INSERT INTO genres (genreName) VALUES (?)";
+        try{
+            Connection connection = connect();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, genreName);
+            preparedStatement.executeUpdate();
+            System.out.println("The genre was successfully added to the database.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    //Delete methods
+
     private static void inputDeleteMovie() {
         System.out.println("Enter movie ID:");
         int movieId = scanner.nextInt();
+        scanner.nextLine();
         deleteMovie(movieId);
     }
 
@@ -70,129 +199,26 @@ public class Main {
         }
     }
 
-    private static void inputAddMovie() {
-        // Add genre option
-        try {
-            System.out.println("Enter movie name: ");
-            String title = scanner.nextLine();
-            System.out.println("Enter movie director: ");
-            String director = scanner.nextLine();
-            System.out.println("Enter movie year: ");
-            int year = Integer.parseInt(scanner.nextLine());
-            System.out.println("enter movie genre: ");
-            String genreIn = scanner.nextLine();
-            int genreOut = checkGenre(genreIn);
-            addMovie(title, director, year, genreOut);
-        } catch (Exception e) {
-            System.out.println("Woops, something went wrong.");
-        }
+    private static void inputDeleteGenre() {
+        System.out.println("Enter genre ID:");
+        int genreId = scanner.nextInt();
+        scanner.nextLine();
+        deleteGenre(genreId);
     }
 
-    private static int checkGenre(String genreIn) {
-        String sql = "SELECT genreId FROM genres WHERE genreName = ?";
-        int genreOut = 0;
+    private static void deleteGenre(int genreId) {
+        String sql = "DELETE FROM genres WHERE genreId = ?";
         try{
             Connection connection = connect();
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, genreIn);
+            preparedStatement.setInt(1, genreId);
             preparedStatement.executeUpdate();
-            ResultSet resultSet = preparedStatement.getResultSet();
-            int genreTemp = resultSet.getInt("genreId");
-
-        } catch (SQLException e){
-            genreOut = addGenre(genreIn);
-        }
-
-        return genreOut;
-
-    }
-
-    private static int addGenre(String genreIn) {
-        String sql = "INSERT INTO genres (genreName) VALUES (?)";
-        int genreOut = 0;
-        try{
-            Connection connection = connect();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, genreIn);
-            preparedStatement.executeUpdate();
-            ResultSet resultSet = preparedStatement.getResultSet();
-            genreOut = resultSet.getInt("genreId");
-        } catch (SQLException e){
-            System.out.println(e.getMessage());
-        }
-        return genreOut;
-    }
-
-    private static void addMovie(String title, String director, int year, int genre) {
-
-        String sql = "INSERT INTO movies (movieTitle, movieDirector, movieReleaseYear, movieGenreId) VALUES (?,?,?,?)";
-
-        try{
-            Connection connection = connect();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, title);
-            preparedStatement.setString(2, director);
-            preparedStatement.setInt(3, year);
-            preparedStatement.executeUpdate();
-            System.out.println("The movie was successfully added to the database.");
+            System.out.println("The genre has been deleted");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-
     }
 
-    private static void showAllMovies() {
-        String sql = "SELECT *FROM movies";
-
-        try{
-            Connection connection = connect();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            printAllMovies(resultSet);
-
-        } catch (SQLException e){
-            System.out.println(e.getMessage());
-        }
-
-    }
-
-    private static void printAllMovies(ResultSet resultSet) throws SQLException {
-        System.out.println();
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("movieId") + "\t"
-                    + resultSet.getString("movieTitle") + "\t"
-                    + resultSet.getString("movieDirector") + "\t"
-                    + resultSet.getString("movieReleaseYear"));
-        }
-    }
-
-    private static int consoleInput() {
-        int input = 0;
-        while (true) {
-            try {
-                input = Integer.valueOf(scanner.nextLine());
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Please choose one of the following options:");
-                printConsole();
-            }
-        }
-        return input;
-    }
-
-    private static void printConsole() {
-        System.out.println("\nChoose:\n");
-        System.out.println(
-                "1  - Show all watched movies \n" +
-                "2  - Show genres watched\n" +
-                "3  - Show favorites\n" +
-                "4  - Add a movie\n" +
-                "5  - Delete a movie\n" +
-                "6  - Search\n" +        
-                "0  - Exit"
-        );
-    }
 
     private static Connection connect() {
         // SQLite connection string
